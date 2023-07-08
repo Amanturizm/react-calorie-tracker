5759e489872f9e238efb927d919fcbb712edecd5
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-07-08 18:55:32 +0600

    add total calories

--- src/containers/CalorieTracker/CalorieTracker.tsx
+++ src/containers/CalorieTracker/CalorieTracker.tsx
@@ -5,13 +5,15 @@ import {Link} from "react-router-dom";
 
 const CalorieTracker = () => {
   const [meals, setMeals] = useState<IMealInfo[]>([]);
+  const [totalCalories, setTotalCalories] = useState<number>(0);
 
   const fetchData = useCallback(async () => {
     try {
       const { data } = await axiosApi.get<IApiMealInfo>('/meals.json');
 
-      const formattedData = data ? Object.keys(data).map(id => ({ ...data[id], id })) : [];
+      const formattedData = data ? Object.keys(data).map(id => ({ ...data[id], kcal: +data[id].kcal, id })) : [];
 
+      setTotalCalories(formattedData.reduce((acc, meal) => acc + meal.kcal, 0));
       setMeals(formattedData);
     } catch (e) {
       console.error(e);
@@ -23,7 +25,7 @@ const CalorieTracker = () => {
   return (
     <div className="d-flex flex-column gap-4 mt-4">
       <div className="d-flex justify-content-between align-items-center w-75 mx-auto">
-        <h5>Total calories: 900 kcal</h5>
+        <h5>Total calories: {totalCalories} kcal</h5>
         <Link to="new-meal" className="btn btn-primary">Add new meal</Link>
       </div>
 
